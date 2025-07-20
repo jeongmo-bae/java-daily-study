47d0ed054f0a147af6160f186eab7defdf04df6a
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-07-20 20:49:33 +0900

    chore : add 0-5-maven-demo maven project

---
+++ 0-week-build-bootcamp/0-5-maven-demo/src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);  // 항상 true → 테스트 통과 확인용
+    }
+}
