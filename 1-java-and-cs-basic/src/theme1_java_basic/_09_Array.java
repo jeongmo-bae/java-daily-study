eb4dd1e810663d1ec8a7f812eff46b0ec22e5de6
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-08-09 15:09:55 +0900

    feat : study Array of theme1_java_basic

--- 1-java-and-cs-basic/src/theme1_java_basic/_09_Array.java
+++ 1-java-and-cs-basic/src/theme1_java_basic/_09_Array.java
@@ -1,4 +1,29 @@
 package theme1_java_basic;
 
+import java.util.Arrays;
+
 public class _09_Array {
+    public static void main(String[] args) {
+        String p1 = "아메리카노";
+        String p2 = "라떼";
+        String p3 = "카페모카";
+        // 베열 선언1
+        String[] coffees = new  String[3];
+        // 배열 선언2
+        String[] coffees2  = new String[] {p1,p2,p3};
+        System.out.println(coffees2[0]);
+        // 배열 선언3
+        String[] coffees3  = {p1,p2,p3};
+        System.out.println(coffees3[1]);
+        System.out.println("================================");
+        // 배열 순회
+        for (int i = 0; i < coffees3.length ; i++) {
+            System.out.println(coffees3[i]);
+        }
+        System.out.println("================================");
+        // enhanced for(for each)
+        for(String coffee : coffees3){
+            System.out.println(coffee);
+        }
+    }
 }
