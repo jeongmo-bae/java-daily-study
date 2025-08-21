79388d1d2643167a070058ed7258fe7bff051cfa
jeongmo-bae <jungmob1024@gmail.com>
2025-08-21 03:32:51 +0000

    feat : study lambda

--- 1-java-and-cs-basic/src/theme5_java_functional_programming/lambda/Procedure.java
+++ 1-java-and-cs-basic/src/theme5_java_functional_programming/lambda/Procedure.java
@@ -1,5 +1,7 @@
 package theme5_java_functional_programming.lambda;
 
+// 정확히 하나의 추상 메서드를 가지는 인터페이스 (SAM : Single Abstract Method)
+// FunctionalInterface Annotation : Compile 단계에서 Interface of SAM 보장을 위해 사용
 @FunctionalInterface
 public interface Procedure {
     void run();
---
+++ 1-java-and-cs-basic/src/theme5_java_functional_programming/lambda/lambda1/InstanceMain.java
@@ -0,0 +1,34 @@
+package theme5_java_functional_programming.lambda.lambda1;
+
+import testPackage._01_AccessModifierTest;
+import theme5_java_functional_programming.lambda.Procedure;
+
+public class InstanceMain {
+    public static void main(String[] args) {
+        Procedure procedure1 = new Procedure() {
+            @Override
+            public void run(){
+                System.out.println("\n=========InstanceMainOfLambda1==========");
+            }
+        };
+        procedure1.run();
+        System.out.println("class.class = " + procedure1.getClass());
+        System.out.println("class.instance = " + procedure1);
+        
+        Procedure procedure2 = () -> {
+            System.out.println("\n=========InstanceMainOfLambda2==========");
+        };
+        procedure2.run();
+        System.out.println("lambda.class = " + procedure2.getClass());
+        System.out.println("lambda.instance = " + procedure2);
+        
+    }
+}
+
+// =========InstanceMainOfLambda1==========
+// class.class = class theme5_java_functional_programming.lambda.lambda1.InstanceMain$1
+// class.instance = theme5_java_functional_programming.lambda.lambda1.InstanceMain$1@16b4a017
+
+// =========InstanceMainOfLambda2==========
+// lambda.class = class theme5_java_functional_programming.lambda.lambda1.InstanceMain$ $Lambda$1/0x0000000800063c40
+// lambda.instance = theme5_java_functional_programming.lambda.lambda1.InstanceMain$ $Lambda$1/0x0000000800063c40 @6842775d
